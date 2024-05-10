8985d34e56f6282e3fbf0567002468e7649fa020
antoniocukrov <antonio.cukrov1@gmail.com>
2024-05-10 13:37:54 +0200

    Broj slova u stringu

--- Program.cs
+++ Program.cs
@@ -17,23 +17,24 @@ namespace ModulOne
             Console.WriteLine("U nastavku molimo odaberite Vaš zahtjev i potvrdite tipkom ENTER.\n");
 
             Console.Write("(1.)Pretvori sva slova imena i prezimena u velika slova.\n" +
-                          "(2.)Pretvori sva slova imena i prezimena u velika slova.\n" +
-                          "(3.)Pretvori sva slova imena i prezimena u velika slova.\n" +
-                          "(4.)Pretvori sva slova imena i prezimena u velika slova.\n" +
-                          "(5.)Pretvori sva slova imena i prezimena u velika slova.\n" +
-                          "(6.)Pretvori sva slova imena i prezimena u velika slova.\n" +
-                          "(7.)Pretvori sva slova imena i prezimena u velika slova.\n" +
+                          "(2.)Saznaj koliko ima slova u tvome imenu i prezimenu.\n" +
+                          "(3.)Obrni svoje ime i prezime.\n" +
+                          "(4.)Pretvori svoje ime i prezime u H4CK3R 5P33K!\n" +
+                          "(5.)Saznaj svoje inicijale.\n" +
+                          "(6.)Saznaj trenutan datum i vrijeme.\n" +
+                          "(7.)Saznaj vremensku temperaturu u Hrvatskoj.\n" +
                           "Izbor:");
 
             int izbor = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("");
 
             switch (izbor)
             {
                 case 1:
-                    Console.WriteLine("Hello");
+                    Console.WriteLine(VelikoSlovo(ime, prezime));
                     break;
                 case 2:
-                    Console.WriteLine("Hello");
+                    Console.WriteLine("U tvome imenu i prezimenu ima {0} slova.", BrojMiSlova(ime,prezime));
                     break;
                 case 3:
                     Console.WriteLine("Hello");
@@ -57,5 +58,16 @@ namespace ModulOne
 
             Console.ReadKey();
         }
+
+        static string VelikoSlovo(string i, string j)
+        {
+            return i.ToUpper() + " " + j.ToUpper();
+        }
+
+        static int BrojMiSlova(string i, string j)
+        {
+            return i.Length + j.Length;
+        }
+        
     }
 }
