e6c35e43295e9dec516e03e5d77a11a738cc821a
antoniocukrov <antonio.cukrov1@gmail.com>
2024-05-10 19:26:38 +0200

    Ostale metode ubačene za odabrani kut

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ namespace ModulOne
              Korisnik unosi dva broja koja određuju dimenzije dvodimenzionalnog polja u kojem se ispisuju brojevi
              ciklički prema sredini počevši od određenog kuta.
             */
-
             Console.WriteLine("Geeks Incorporated ® ™©\n\nMolimo unesite dva broja koja određuju visinu i širinu polja.");
             Console.Write("Širina polja:");
             int x = Int32.Parse(Console.ReadLine());
@@ -20,15 +19,12 @@ namespace ModulOne
             int ix = 0, iy = 0;
             int[,] poljeGeek = new int[x, y];
 
-            GoreLijevo(x, y, poljeGeek, counter, ix, iy);
-
+            DoljeLijevo(x, y, poljeGeek, counter, ix, iy);
 
             IspisiTablu(x, y, poljeGeek);
 
             Console.ReadLine();
-
         }
-
         static void IspisiTablu(int x, int y, int[,] poljeGeek)
         {
             for (int ix = 0; ix < x; ix++)
@@ -40,7 +36,6 @@ namespace ModulOne
                 }
             }
         }
-
         static void GoreLijevo(int x, int y, int[,] poljeGeek, int counter, int ix, int iy)
         {
             x--;
@@ -58,7 +53,6 @@ namespace ModulOne
                     }
                     poljeGeek[ix, iy] = counter;
                 }
-
                 //STEP 2 - Piši dolje
                 for (; ix < (x - listic); ix++)
                 {
@@ -69,8 +63,6 @@ namespace ModulOne
                     }
                     poljeGeek[ix, iy] = counter;
                 }
-
-
                 //STEP 3 - Piši lijevo
                 for (; iy >= listic; iy--)
                 {
@@ -84,7 +76,6 @@ namespace ModulOne
                 iy++;
                 ix--;
                 listic++;
-
                 //STEP 4 - Piši gore
                 for (; ix >= listic; ix--)
                 {
@@ -94,28 +85,183 @@ namespace ModulOne
                         break;
                     }
                     poljeGeek[ix, iy] = counter;
+                }
+                ix++;
+                iy++;
+            }
+        }
 
+        static void GoreDesno(int x, int y, int[,] poljeGeek, int counter, int ix, int iy)
+        {
+            x--;
+            y--;
+            int listic = 0;
+            iy = y;
+            while (counter <= ((x + 1) * (y + 1)))
+            {
+                //STEP 1 - Piši dolje
+                for (; ix < (x - listic); ix++)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+                //STEP 2 - Piši lijevo
+                for (; iy >= listic; iy--)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+                iy++;
+                ix--;
+                //STEP 3 - Piši gore
+                for (; ix >= listic; ix--)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+                ix++;
+                iy++;
+                listic++;
+                //STEP 4 - Piši desno
+                for (; iy < (y - listic); iy++)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+            }
+        }
 
+        static void DoljeDesno(int x, int y, int[,] poljeGeek, int counter, int ix, int iy)
+        {
+            x--;
+            y--;
+            int listic = 0;
+            iy = y;
+            ix = x;
+            while (counter <= ((x + 1) * (y + 1)))
+            {
+                //STEP 1 - Piši lijevo
+                for (; iy >= listic; iy--)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+                iy++;
+                ix--;
+
+                //STEP 2 - Piši gore
+                for (; ix >= listic; ix--)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
                 }
                 ix++;
                 iy++;
 
+                //STEP 3 - Piši desno
+                for (; iy < (y - listic); iy++)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+                listic++;
 
+                //STEP 4 - Piši dolje
+                for (; ix < (x - listic); ix++)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
             }
-
-
         }
-
-        /*static void Pisidolje(int x, int y, int[,] poljeGeek, int counter, int ix = 0, int iy = 0)
+        static void DoljeLijevo(int x, int y, int[,] poljeGeek, int counter, int ix, int iy)
         {
-            for (; ix < x; ix++)
+            x--;
+            y--;
+            int listic = 0;
+            ix = x;
+            while (counter <= ((x + 1) * (y + 1)))
             {
-                counter++;
-                poljeGeek[ix, iy] = counter;
-            }
-            x++;
-        }
-        */
+                //STEP 1 - Piši gore
+                for (; ix >= listic; ix--)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+                ix++;
+                iy++;
 
+                //STEP 2 - Piši desno
+                for (; iy < (y - listic); iy++)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+
+                //STEP 3 - Piši dolje
+                for (; ix < (x - listic); ix++)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+                listic++;
+                //STEP 4 - Piši lijevo
+                for (; iy >= listic; iy--)
+                {
+                    counter++;
+                    if (counter > ((x + 1) * (y + 1)))
+                    {
+                        break;
+                    }
+                    poljeGeek[ix, iy] = counter;
+                }
+                iy++;
+                ix--;
+            }
+        }
     }
 }
